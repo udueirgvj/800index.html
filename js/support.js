2017bc6f39ba88f4561a68560540a248bbc5cd39
udueirgvj <lllsgl8j@gmail.com>
2026-02-15 18:31:02 +0300

    Delete js/support.js

--- js/support.js
+++
@@ -1,5 +0,0 @@
-const Support = {
-    openSupportPanel() { document.getElementById('supportPanel').classList.add('open'); },
-    closeSupportPanel() { document.getElementById('supportPanel').classList.remove('open'); }
-};
-window.Support = Support;
