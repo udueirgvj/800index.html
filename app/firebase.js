3f2d14eab764553686da27b10e8d5fc3cb46f6bf
udueirgvj <lllsgl8j@gmail.com>
2026-02-17 08:07:30 +0300

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,17 +1,22 @@
-// Firebase CDN (النسخة المتوافقة مع HTML العادي)
-const firebaseConfig = {
+<!-- Firebase core -->
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+
+<!-- Auth -->
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+
+<!-- Database -->
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
+
+<script>
+var firebaseConfig = {
   apiKey: "AIzaSyDRCtfuYrEdnuKUsWu_79NC6G_xGLznBJc",
   authDomain: "tttrt-b8c5a.firebaseapp.com",
   databaseURL: "https://tttrt-b8c5a-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "tttrt-b8c5a",
-  storageBucket: "tttrt-b8c5a.firebasestorage.app",
+  storageBucket: "tttrt-b8c5a.appspot.com",
   messagingSenderId: "975123752593",
   appId: "1:975123752593:web:e591e930af3a3e29568130"
 };
 
-// تشغيل فايربيس
 firebase.initializeApp(firebaseConfig);
-
-// الخدمات
-const auth = firebase.auth();
-const db = firebase.database();
+</script>
