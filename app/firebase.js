0af0a79de7d8a2eff213dc3812e1c463e9aa3017
udueirgvj <lllsgl8j@gmail.com>
2026-02-17 07:55:19 +0300

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,13 +1,17 @@
-// بيانات مشروعك من Firebase
+// Firebase CDN (النسخة المتوافقة مع HTML العادي)
 const firebaseConfig = {
-  apiKey: "PUT_API_KEY_HERE",
+  apiKey: "AIzaSyDRCtfuYrEdnuKUsWu_79NC6G_xGLznBJc",
   authDomain: "tttrt-b8c5a.firebaseapp.com",
-  databaseURL: "https://tttrt-b8c5a-default-rtdb.firebaseio.com",
+  databaseURL: "https://tttrt-b8c5a-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "tttrt-b8c5a",
-  storageBucket: "tttrt-b8c5a.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:000000000000"
+  storageBucket: "tttrt-b8c5a.firebasestorage.app",
+  messagingSenderId: "975123752593",
+  appId: "1:975123752593:web:e591e930af3a3e29568130"
 };
 
-// تشغيل Firebase
+// تشغيل فايربيس
 firebase.initializeApp(firebaseConfig);
+
+// الخدمات
+const auth = firebase.auth();
+const db = firebase.database();
